291ac4ef67e5a58b4e72bd3307601b86e73e439b
DennyNo1 <zht2041359@vip.qq.com>
2023-08-25 17:00:57 +0800

    day2

---
+++ day02/scripts/main.js
@@ -0,0 +1,49 @@
+/*let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("我最喜欢巧克力冰激淋了。");
+} else {
+  alert("但是巧克力才是我的最爱呀……");
+}
+document.querySelector("html").addEventListener("click", () => {
+    alert("别戳我，我怕疼。");
+  });*/
+
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+  }
+  
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  }
+
+  myButton.onclick = function () {
+    setUserName();
+  };
+  
