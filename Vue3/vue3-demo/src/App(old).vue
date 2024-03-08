f718a227f4601963e44cfe322eb507546be2f21c
DennyNo1 <zht2041359@vip.qq.com>
2024-03-08 17:00:25 +0800

    开始学vue3

---
+++ Vue3/vue3-demo/src/App(old).vue
@@ -0,0 +1,32 @@
+
+<script>
+//1.setup的执行时机，比beforeCreate还早
+//2.setup函数中，获取不到this，因为时间太早了，this即组件实例还没被创建出来
+//3. 数据和函数，需要在setup最后return，才能在模板中应用
+export default {
+  
+  
+  setup(){
+    //console.log('setup函数')
+
+    //变量
+    const message='hello world'
+    //函数
+    const logMessage=()=>{
+      console.log(message)
+    }
+    return{
+      message,logMessage
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate函数')
+  }
+}
+</script>
+<template>
+  <div>
+    {{ message }}
+  </div>
+  <button @click="logMessage">打印信息</button>
+</template>
---
+++ Vue3/vue3-demo/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// reactive:接收一个对象类型的数据，返回一个响应式的对象
+// ref:接收简单类型或者对象类型的数据的传入，并返回一个响应式的对象。
+// 本质是将简单类型的数据转换成对象，该对象.value接收原数据。
+import {reactive,ref,computed,watch} from 'vue'
+import Computed from './components/Computed.vue'
+//大括号代表对象
+    const object = reactive({
+        count:100
+    })
+    const addObject = ()=>{
+        object.count++;
+        object1.value++;
+    }
+    const object1= ref(0)
+
+    const name=ref(0)
+
+    watch(){
+        
+    }
+
+
+    
+</script>
+<template>
+    <div>
+        {{object.count}}
+        {{object1}}
+        <button @click="addObject">+1</button>
+    </div>
+    <Computed></Computed>
+    <div>
+
+    </div>
+</template>
